fe54f9f1c2fb18ac617601c3f822eedab3101f09
Prosper226 <psedgo@gmail.com>
2023-03-03 10:29:58 +0000

    coinbase: balance, sendMoney

--- src/COINBASE/Coinbase.php
+++ src/COINBASE/Coinbase.php
@@ -39,45 +39,15 @@ class Coinbase {
         }
     }
 
-    public function create_address($code){
+    public function create_address($code, $network = null){
         try{
             if(!isset($code) || !$code) throw new Exception('code param is mandatory.');
-            $res = $this->manager->create_address($code);
+            $res = $this->manager->create_address($code, $network);
             return ["code" => 200, "data" => $res];
         }catch(Exception $e){
             return ["code" => 412, "error" => $e->getMessage()];
         }
     }
-
-    // public function get_account_id($code = null){
-    //     try{
-    //         if(!isset($code) || !$code) throw new Exception('code param is mandatory.');
-    //         $res = $this->manager->get_account_id($code);
-    //         return ["code" => 200, "data" => $res];
-    //     }catch(Exception $e){
-    //         return ["code" => 412, "error" => $e->getMessage()];
-    //     }
-    // }
-    
-    // public function show_an_account($code){
-    //     try{
-    //         if(!isset($code) || !$code) throw new Exception('code param is mandatory.');
-    //         $res = $this->manager->show_an_account($code);
-    //         return ["code" => 200, "data" => $res];
-    //     }catch(Exception $e){
-    //         return ["code" => 412, "error" => $e->getMessage()];
-    //     }
-    // }
-
-    // public function list_addresses($code){
-    //     try{
-    //         if(!isset($code) || !$code) throw new Exception('code param is mandatory.');
-    //         $res = $this->manager->list_addresses($code);
-    //         return ["code" => 200, "data" => $res];
-    //     }catch(Exception $e){
-    //         return ["code" => 412, "error" => $e->getMessage()];
-    //     }
-    // }
 
     public function list_transactions($code){
         try{
@@ -91,7 +61,9 @@ class Coinbase {
 
     public function check_transaction($code, $address, $amount){
         try{
-            // if(!isset($code) || !$code) throw new Exception('code param is mandatory.');
+            if(!isset($code) || !$code) throw new Exception('code param is mandatory.');
+            if(!isset($address) || !$address) throw new Exception('address param is mandatory.');
+            if(!isset($amount) || !$amount) throw new Exception('amount param is mandatory.');
             $res = $this->manager->check_transaction($code, $address, $amount);
             return ["code" => 200, "data" => $res];
         }catch(Exception $e){
@@ -99,7 +71,28 @@ class Coinbase {
         }
     }
 
-    
+    public function balance($code = null){
+        try{
+            if(!isset($code) || !$code) throw new Exception('code param is mandatory.');
+            $res = $this->manager->get_account_balance($code);
+            return ["code" => 200, "data" => $res];
+        }catch(Exception $e){
+            return ["code" => 412, "error" => $e->getMessage()];
+        }
+    }
+
+    public function send_monney($code, $recepteur, $amount, $memo = null, $idem = null){
+        try{
+            if(!isset($code) || !$code) throw new Exception('code param is mandatory.');
+            if(!isset($recepteur) || !$recepteur) throw new Exception('destination address is missing.');
+            if(!isset($amount) || !$amount) throw new Exception('amount to send is missing.');
+            $res = $this->manager->send_monney($code, $recepteur, $amount, $memo, $idem);
+            return ["code" => 200, "data" => $res];
+        }catch(Exception $e){
+            return ["code" => 412, "error" => $e->getMessage()];
+        }
+    }
+
 }
 
 
